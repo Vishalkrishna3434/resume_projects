d6fc2da0f20e4024228c413bd5abd420574f9f15
Vishalkrishna3434 <vishalthonukunooru@gmail.com>
2026-03-11 01:57:11 +0530

    created routes,controllers and middleware

---
+++ backend/Controllers/GoalController.js
@@ -0,0 +1,38 @@
+//@desc Set goals
+//@route GET /api/goals
+//@access Private
+const getGoals=(req,res)=>{
+  res.status(200).json({message :"get Goals"});
+}
+
+//@desc Set goal
+//@route POST /api/goals
+//@access Private
+const setGoal=(req,res)=>{
+  if(!req.body || !req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field');
+  }
+  res.status(200).json({message :"Put goals"});
+}
+
+//@desc Update goal
+//@route PUT /api/goals/:id
+//@access Private
+const updateGoal=(req,res)=>{
+  res.status(200).json({message :`update goals ${req.params.id}`});
+}
+
+//@desc Delete goal
+//@route Delete /api/goals?:id
+//@access Private
+const deleteGoal=(req,res)=>{
+  res.status(200).json({message :`delete goals ${req.params.id}`});
+}
+
+module.exports={
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const express=require("express");
+const dotenv=require("dotenv").config();
+const port= process.env.PORT || 5000;
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const userRoutes=require("./Routes/GoalRoutes");
+const {errorHandler}=require("./middleware/errorMiddleware");
+
+app.use("/api/goals",userRoutes);
+app.use(errorHandler);
+
+app.listen(port,()=> console.log(`process is taking place in port ${port}`));
